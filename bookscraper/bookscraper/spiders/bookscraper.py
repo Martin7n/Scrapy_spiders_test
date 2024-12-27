c759675c1afe1f01d0db407a1adec4ec3cef9d67
Martin7n <122726440+Martin7n@users.noreply.github.com>
2024-12-27 17:50:58 +0200

    next pages scan

--- bookscraper/bookscraper/spiders/bookscraper.py
+++ bookscraper/bookscraper/spiders/bookscraper.py
@@ -1,11 +1,16 @@
+from gc import callbacks
+
 import scrapy
 from scrapy.spiders import CrawlSpider
+
 
 
 class BookScraper(scrapy.Spider):
     name = 'bookscraper'
     allowed_domains = ['books.toscrape.com']
     start_urls = ['http://books.toscrape.com/']
+
+
 
     def parse(self, response):
         prods =  response.css(".product_pod")
@@ -18,5 +23,13 @@ class BookScraper(scrapy.Spider):
                 'availability': product.css('p.instock.availability::text').getall(),
             }
 
+        next = response.css('li.next>a').attrib['href']
 
+
+        if next is not None:
+            if 'catalogue' in next:
+                next_page_url = self.start_urls[0] + next
+            else:
+                next_page_url =  self.start_urls[0] + "catalogue/" + next
+            yield response.follow(next_page_url, callback=self.parse)
 
